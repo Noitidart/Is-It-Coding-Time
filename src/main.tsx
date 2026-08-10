97b7bd3aa0b559078f6a20c3f4a4432b7509bb0e
Noitidart <noitidart@protonmail.com>
2026-08-10 04:31:36 -0700

    Remove non-Tailwind CSS that was breaking Tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <h1 className="text-3xl font-bold underline">Hello world!</h1>
-
-    <App />
   </StrictMode>
 );
